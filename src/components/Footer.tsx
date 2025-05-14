3ed5124bb25c99ef3ad16925f1be4a2711539e0d
Clients <client@Mac.attlocal.net>
2025-05-14 10:54:32 -0700

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="bg-white text-center py-4 mt-8 text-sm text-gray-500">
+    <footer className="bg-white text-center drop-shadow-[0_-4px_6px_rgba(0,0,0,0.2)] py-4 mt-8 text-md text-gray-500">
       <p>© 2025 Kudoboard</p>
     </footer>
   );
